23cbd676da6e09497a4d4ef623a12bb6ee0495df
Ollári Márton <ollarimarton@gmail.com>
2018-09-07 14:09:46 +0200

    added date to transaction

--- src/main/java/com/codecool/supercharge/model/Transaction.java
+++ src/main/java/com/codecool/supercharge/model/Transaction.java
@@ -1,6 +1,9 @@
 package com.codecool.supercharge.model;
 
+import com.sun.xml.internal.ws.spi.db.DatabindingException;
+
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Transaction {
@@ -12,6 +15,7 @@ public class Transaction {
     @JoinColumn(name = "account_id")
     private Account account;
 
+    private Date date;
     private String event;
 
     private int change;
