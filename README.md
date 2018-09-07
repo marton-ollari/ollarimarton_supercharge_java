68ef621129f34c01e435a5087742d8308bd0807f
Ollári Márton <ollarimarton@gmail.com>
2018-09-07 14:35:18 +0200

    created basic controller and readme file

---
+++ README.md
@@ -0,0 +1,1 @@
+Bank Controller 1.0
--- src/main/java/com/codecool/supercharge/controller/SuperchargeController.java
+++ src/main/java/com/codecool/supercharge/controller/SuperchargeController.java
@@ -1,4 +1,28 @@
 package com.codecool.supercharge.controller;
 
+import com.codecool.supercharge.service.AccountService;
+import com.codecool.supercharge.service.TransactionService;
+import com.codecool.supercharge.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class SuperchargeController {
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    TransactionService transactionService;
+
+    @ResponseBody
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String mainpage(){
+        return "Bank Controller 1.0 - See README";
+    }
 }
